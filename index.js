c6c040a22a46140201334c044a29d808c3893b98
Roman Shtylman <shtylman@gmail.com>
2012-05-10 21:45:03 -0400

    when log file moves make a new file with original name
    - supports moving log files to rotate them

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ module.exports = function(opt) {
         throw new Error('filename required for book-file transport');
     }
 
-    var write_stream = fs.createWriteStream(filename, {
-        flags: 'a',
-        encoding: 'utf8'
-    });
+    // dont handle renamed file
+    var no_move = opt.no_move;
+
+    var write_stream;
+
+    (function new_log_file() {
+        write_stream = fs.createWriteStream(filename, {
+            flags: 'a',
+            encoding: 'utf8'
+        });
+
+        if (!no_move) {
+            var watcher = fs.watch(filename, function(ev) {
+                if (ev === 'rename') {
+                    // stop watching this fd
+                    watcher.close();
+
+                    // init new file
+                    new_log_file();
+                }
+            });
+        }
+    })();
 
     /// actual logging work
     return function() {
